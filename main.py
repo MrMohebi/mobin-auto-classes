921eff1246dc4d928989d2159f0e098e0d3023da
MrMohebi <MMMohebi@outlook.com>
2021-09-24 15:21:05 +0330

    start recording on classes

--- main.py
+++ main.py
@@ -1,56 +1,90 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import datetime
 import pyautogui
 from time import sleep
-from threading import Thread
 
 
 BASE_URL="https://mobin.uk.ac.ir/"
-USERNAME="test"
-PASSWORD="testPass"
+USERNAME="testUsername"
+PASSWORD="testPassword"
 
-# cancel certificate pop up
-def threaded_function2():
-    sleep(10)
-    pyautogui.press('tab')
-    pyautogui.press('right')
-    pyautogui.press('right')
-    pyautogui.press('enter')
-thread2 = Thread(target = threaded_function2)
-thread2.start()
+classTime = {
+    "Saturday":[9,13, 15],
+    "Sunday":[9, 11],
+    "Monday":[11, 13, 15],
+    "Tuesday":[11],
+    "Wednesday":[9, 11, 13],
+    "Thursday":[],
+    "Friday":[]
+}
 
-# open site
-driver = webdriver.Chrome()
-driver.get(BASE_URL)
-driver.maximize_window()
+isRecording = False
 
-# login
-usernameFiled = driver.find_element_by_name("UserName")
-usernameFiled.send_keys(USERNAME)
-passwordFiled = driver.find_element_by_name("Password")
-passwordFiled.send_keys(PASSWORD)
-passwordFiled.send_keys(Keys.RETURN)
+# selenium web driver holder
+driver = webdriver.Chrome("chromedriver")
 
-# go to open class
-mobinWindowId = driver.current_window_handle
-enterClass = driver.find_element_by_link_text('ورود')
-enterClass.click()
+# check each 5 min
+while True:
+    now = datetime.datetime.now()
+    todayClasses = classTime[now.strftime("%A")]
+    if (now.hour in todayClasses) and (36 < now.minute < 24) and (isRecording == False):
+        # cancel certificate pop up
+        # def threaded_function2():
+        #     sleep(10)
+        #     pyautogui.press('tab')
+        #     pyautogui.press('right')
+        #     pyautogui.press('right')
+        #     pyautogui.press('enter')
+        # thread2 = Thread(target = threaded_function2)
+        # thread2.start()
 
-#change default window to new tab(select open in web or app)
-chwd = driver.window_handles
-for w in chwd:
-    if w != mobinWindowId:
-        driver.switch_to.window(w)
+        # open site
+        driver = webdriver.Chrome("chromedriver")
+        driver.get(BASE_URL)
+        driver.maximize_window()
 
-# open class in web
-driver.execute_script("openInBrowser()")
+        sleep(10)
 
-# # open class in app
-# driver.execute_script("openInApplication()")
-# sleep(5)
-# pyautogui.press('tab')
-# pyautogui.press('tab')
-# pyautogui.press('enter')
+        # login
+        usernameFiled = driver.find_element_by_name("UserName")
+        usernameFiled.send_keys(USERNAME)
+        passwordFiled = driver.find_element_by_name("Password")
+        passwordFiled.send_keys(PASSWORD)
+        passwordFiled.send_keys(Keys.RETURN)
 
+        sleep(10)
+
+        # go to open class
+        mobinWindowId = driver.current_window_handle
+        enterClass = driver.find_element_by_link_text('ورود')
+        enterClass.click()
+
+        #change default window to new tab(select open in web or app)
+        chwd = driver.window_handles
+        for w in chwd:
+            if w != mobinWindowId:
+                driver.switch_to.window(w)
+
+        # open class in web
+        driver.execute_script("openInBrowser()")
+
+        # # open class in app
+        # driver.execute_script("openInApplication()")
+        # sleep(5)
+        # pyautogui.press('tab')
+        # pyautogui.press('tab')
+        # pyautogui.press('enter')
+
+        # start recording
+        pyautogui.press('f8')
+        isRecording = True
+
+    elif (now.hour-2 in todayClasses) and (36 < now.minute < 24) and (isRecording == True):
+        pyautogui.press('f8')
+        isRecording = False
+        driver.quit()
+
+    sleep(300)
 
 
